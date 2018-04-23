e25289af2a2f8adc0e2a3e65258da42b51a745ca
Gabriel Miranda <gabrielcm.br@gmail.com>
2018-04-23 19:24:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ function takeANumber(line, name) {
   line.push(name);
   return `Welcome, ${name}. You are number ${line.length} in line.`;
 }
-function nowServing() {
-  if (katzDeliLine.length === 0) {
+function nowServing(line) {
+  if (line.length === 0) {
     return "There is nobody waiting to be served!";
   }
   else {
-    var served = katzDeliLine.slice(0, 1);
-    delete katzDeliLine[0];
+    var served = line.slice(0, 1);
+    delete line[0];
     return `Currently serving ${served}.`;
   }
 }
