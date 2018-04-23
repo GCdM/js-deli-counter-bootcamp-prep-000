3cd0c963b83b91e24caa9c228691e474c4156a95
Gabriel Miranda <gabrielcm.br@gmail.com>
2018-04-23 19:14:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ var katzDeliLine = [];
 
 function takeANumber(line, name) {
   katzDeliLine.push(name);
-  return `Welcome, ${name}. You are number ${katzDeli.length} in line.`;
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+}
